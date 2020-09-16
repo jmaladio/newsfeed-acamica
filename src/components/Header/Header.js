7b6c3016a41019522540cee28c68290f8a9d9569
Julian Aladio <jmaladio@gmail.com>
2020-09-16 03:52:44 -0300

    First commit, setting up basic components and style

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1 className="logo">News</h1>
+      <input type="text" className="search-input" />
+    </header>
+  );
+};
+
+export default Header;
